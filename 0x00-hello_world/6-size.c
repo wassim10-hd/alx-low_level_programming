0c7ee57a9905a3d818047c659ab01d0ab3e5bce2
wassim <wassimhidri34@gmail.com>
2023-06-15 13:24:39 -0700

    aa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ long int b;
 char c;
 long long int l;
 float f;
-printf("Sise of char :%d byte(s)\n", sizeof(c));
-printf("Sise of int :%d byte(s)\n", sizeof(a));
-printf("Sise of long int :%d byte(s)\n", sizeof(b));
-printf("Sise of long long int :%d byte(s)\n", sizeof(l));
-printf("Size of float:%d byte(s) \n", sizeof(f));
+printf("Sise of char: %d byte(s)\n", sizeof(c));
+printf("Sise of int: %d byte(s)\n", sizeof(a));
+printf("Sise of long int: %d byte(s)\n", sizeof(b));
+printf("Sise of long long int: %d byte(s)\n", sizeof(l));
+printf("Size of float: %d byte(s) \n", sizeof(f));
 return (0);
 }
