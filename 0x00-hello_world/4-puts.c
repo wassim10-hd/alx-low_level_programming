821d752aa430d6316a059c88b25cc11432fb3c3e
wassim <wassimhidri34@gmail.com>
2023-06-15 06:54:36 -0700

    aa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+ * main - this program print the vale inside the function puts
+ * Return: 0 (success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
