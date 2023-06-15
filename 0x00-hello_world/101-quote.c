ce6fc12d304260b52b8e95d1429236bdc80f5b27
wassim <wassimhidri34@gmail.com>
2023-06-15 14:30:57 -0700

    advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - this program print a line
+ * Return:1 (succ)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
