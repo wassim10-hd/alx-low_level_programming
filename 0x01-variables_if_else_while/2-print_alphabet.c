2b685fcd4e568eb520fa50d4e7b6ce7ea4151b1c
wassim <wassimhidri34@gmail.com>
2023-06-16 15:12:17 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - print alphabet
+ * Return: 0 success
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
