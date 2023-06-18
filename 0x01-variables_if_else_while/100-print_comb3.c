8c4c8268901c3635fd54bebb7fdc9b548de971e9
wassim <wassimhidri34@gmail.com>
2023-06-18 16:11:54 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - print combination of two digit
+ * Return:0 succ
+ */
+int main(void)
+{
+	int i, j, a;
+
+	a = 49;
+	for (i = 48; i < 58; i++)
+	{
+		for (j = a; j < 58; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(44);
+				putchar(32);
+			}
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
