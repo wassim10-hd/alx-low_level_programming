94793694b29d664eaab55c61c01d9cd1ad3267bd
wassim <wassimhidri34@gmail.com>
2023-06-19 03:44:52 -0700

    comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main - print combination of two two digit numbers
+ * Return: 0 succ
+ */
+int main(void)
+{
+	int a = 0, b;
+
+	while (a <= 99)
+	{
+		b = a;
+		while (b != a)
+		{
+			putchar((a / 10) + 48);
+			putchar((a % 10) + 48);
+			putchar(32);
+			putchar((b / 10) + 48);
+			putchar((b % 10) + 48);
+			if (a != 98 || b != 99)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
