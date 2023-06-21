2738384a0af35bf617b319fccf39eadc4149eccc
wassim <wassimhidri34@gmail.com>
2023-06-21 02:18:28 -0700

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - fibonacci numbers
+ * Return: 0 succ
+ */
+int main(void)
+{
+	long int n = 0, s = 1, fib, i = 0;
+
+	while (1)
+	{
+		fib = n + s;
+		n = s;
+		s = fib;
+		if (fib > 4000000)
+			break;
+		else
+			i = fib + i;
+	}
+	printf("%ld", i);
+	printf("\n");
+	return (0);
+}
